283c69700bd0d1b2266d9e42c41ef140565647c3
RAGHAV GUPTA <149306772+guptaraghav81@users.noreply.github.com>
2024-01-15 20:34:07 +0530

    Update RandomNumber.py

--- RandomNumber.py
+++ RandomNumber.py
@@ -4,8 +4,6 @@ import random
 n = random.randint(1, 20)
 
 guess = 0
-max_hints = 3  # Change this value to control the number of hints
-
 while guess != n:
     guess = int(input("Enter your guess:"))
 
@@ -13,29 +11,29 @@ while guess != n:
         print("Congratulations! You guessed it right!")
     elif guess < n:
         print("Too low!")
-        hints_given = 0
+
 
         if n % 2 == 0:
             print("Hint: The number is divisible by 2.")
-            hints_given += 1
 
-        if n % 3 == 0 and hints_given < max_hints:
+
+        if n % 3 == 0 :
             print("Hint: The number is divisible by 3.")
-            hints_given += 1
 
-        if n % 5 == 0 and hints_given < max_hints:
+
+        if n % 5 == 0 :
             print("Hint: The number is divisible by 5.")
     elif guess > n:
         print("Too high!")
-        hints_given = 0
+
 
         if guess - n > 5:
             print("Hint: You are quite far from the number.")
-            hints_given += 1
 
-        if n % 4 == 0 and hints_given < max_hints:
+
+        if n % 4 == 0 :
             print("Hint: The number is divisible by 4.")
-            hints_given += 1
 
-        if n % 6 == 0 and hints_given < max_hints:
+
+        if n % 6 == 0 :
             print("Hint: The number is divisible by 6.")
